1dc7ff68b453a302591f99ef8e74910b48d4120f
Kenneth Ramirez <kenneth.ramirez@aplaceformom.com>
2019-11-18 14:20:07 -0600

    changing the getOptions on cdot variant E due to a unexpected change applied to the TYP

--- cypress/models/pages/aplaceformom/cDot/cDotTYE.js
+++ cypress/models/pages/aplaceformom/cDot/cDotTYE.js
@@ -1,12 +1,12 @@
 class cDotTYE{
 
     constructor(){        
-        this.localOptions = ".thank-you-view";  
+        this.localOptions = "#lp-pom-button-80";  
     }
 
     get getlocalOptionsElement(){
         if(this.localOptions){
-            return cy.get(this.localOptions).find('div.progress-icons__cta a').as('locationBtn');
+            return cy.get(this.localOptions).as('locationBtn');
         }else{
             throw new Error("No selector provided to get a next button element");
         } 
